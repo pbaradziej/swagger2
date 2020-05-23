95dbbedec2136de37235b8db0f620f72db9005fd
Paweł Baradziej <44201479+pbaradziej@users.noreply.github.com>
2020-05-23 19:35:02 +0200

    Podstawa projektu, połaczenie z baza danych oraz z swagger

---
+++ src/main/java/com/example/springbootswagger2/SpringRestSwagger2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSwagger2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSwagger2Application.class, args);
+	}
+
+}
